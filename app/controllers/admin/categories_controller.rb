016339a5412bb7b5f2e22c9f8e0503304fb9db3b
Patrick Chu <mingweipatrickc@gmail.com>
2018-10-12 15:59:09 +0000

    added admin authentication to admin controllers

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,7 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], only: [:index, :new, :create]
+
   def index
-    # byebug
     @category = Category.order(id: :desc).all
   end
 
